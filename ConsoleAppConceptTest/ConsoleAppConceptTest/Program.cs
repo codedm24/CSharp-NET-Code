37df9c857c4a80a462bd6e33f247ba5d5a7ace35
codedm24 <mithun.adroit@gmail.com>
2024-10-10 18:53:31 +0530

    updated code

--- ConsoleAppConceptTest/ConsoleAppConceptTest/Program.cs
+++ ConsoleAppConceptTest/ConsoleAppConceptTest/Program.cs
@@ -2,6 +2,15 @@
 {
     internal class Program
     {
+        internal class Person
+        {
+            public string FirstName { get; set; } = string.Empty;
+            public string LastName { get; set; } = string.Empty;
+            public string Title { get; set; } = string.Empty;
+            public string Address { get; set; } = string.Empty;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
@@ -12,7 +21,10 @@
             Console.WriteLine($"Age: {age}, Name: {name}, IsEmployed:{isEmployed}");
             TemporaryGrouping();
             ShortLivedDataStructure();
-            
+            Person newPerson = new();
+            Console.WriteLine($"Person FirstName:{newPerson.FirstName}");
+            Person newPerson1 = new Person { FirstName = "John", LastName = "Doie", Address = "Kolkata" };
+            Console.WriteLine($"Person FirstName:{newPerson1.FirstName}, LastName:{newPerson1.LastName}");
         }
 
         //check new tuple syntax/format similar to python
@@ -33,5 +45,8 @@
             var book = ("1984", "George Orwell", 1949);
             Console.WriteLine($"Title: {book.Item1}, Author: {book.Item2}, Year: {book.Item3}");
         }
+
+
+             
     }
 }
