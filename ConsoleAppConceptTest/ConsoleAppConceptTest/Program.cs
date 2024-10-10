1dc456cffa94f12989314c9a47e02b59de2edc50
codedm24 <mithun.adroit@gmail.com>
2024-10-10 11:10:05 +0530

    Concept test app
    .net concept test code

---
+++ ConsoleAppConceptTest/ConsoleAppConceptTest/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleAppConceptTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var personInfo = GetPersonInfo();
+            var age = personInfo.Item1;
+            var name = personInfo.Item2;
+            var isEmployed = personInfo.Item3;
+        }
+
+        //check new tuple syntax/format similar to python
+        private static (int, string, bool) GetPersonInfo()
+        {
+            return (30, "John Doe", true);
+        }
+
+
+    }
+}
