74d1b776dc507fc3e237847b5b9d17090cec7fac
codedm24 <mithun.adroit@gmail.com>
2024-10-10 11:35:11 +0530

    functions added

--- ConsoleAppConceptTest/ConsoleAppConceptTest/Program.cs
+++ ConsoleAppConceptTest/ConsoleAppConceptTest/Program.cs
@@ -9,6 +9,10 @@
             var age = personInfo.Item1;
             var name = personInfo.Item2;
             var isEmployed = personInfo.Item3;
+            Console.WriteLine($"Age: {age}, Name: {name}, IsEmployed:{isEmployed}");
+            TemporaryGrouping();
+            ShortLivedDataStructure();
+            
         }
 
         //check new tuple syntax/format similar to python
@@ -17,6 +21,17 @@
             return (30, "John Doe", true);
         }
 
+        private static void TemporaryGrouping()
+        {
+            var coordinate = ( x: 5, y: 10);
+            Console.WriteLine($"X: {coordinate.x}, Y: {coordinate.y}");
 
+        }
+
+        private static void ShortLivedDataStructure()
+        {
+            var book = ("1984", "George Orwell", 1949);
+            Console.WriteLine($"Title: {book.Item1}, Author: {book.Item2}, Year: {book.Item3}");
+        }
     }
 }
